2a2d8579cf2073ecd60884cbde9f176a1c7767f4
Elias Salvan <51190948+elsgit43@users.noreply.github.com>
2025-01-14 16:06:23 +0000

    commit

--- export-neo4j.py
+++ export-neo4j.py
@@ -26,6 +26,8 @@ print("Deleting existing nodes and relationships...")
 graph.run("MATCH ()-[r]->() DELETE r")
 graph.run("MATCH (n:Artist) DETACH DELETE n")
 graph.run("MATCH (n:Film) DETACH DELETE n")
+#graph.run("DROP INDEX FOR (f:Film) ON (f.idFilm) IF EXISTS")
+#graph.run("DROP INDEX FOR (a:Artist) ON (a.idArtist) IF EXISTS")
 
 with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
     cursor = conn.cursor()
@@ -44,7 +46,7 @@ with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
         i = 0
         for row in rows:
             # Créer un objet Node avec comme label Film et les propriétés adéquates
-            # A COMPLETER
+            n=Node("Film",idFilm=row[0],primaryTitle=row[1],startYear=row[2])
             importData.append(n)
             i += 1
 
@@ -58,12 +60,35 @@ with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
     # Names
     # En vous basant sur ce qui a été fait dans la section précédente, exportez les données de la table tNames
     # A COMPLETER
+    exportedCount = 0
+    cursor.execute("SELECT COUNT(1) FROM TArtist")
+    totalCount = cursor.fetchval()
+    cursor.execute("SELECT idArtist, primaryName, birthYear FROM TArtist")
+    while True:
+        importData = []
+        rows = cursor.fetchmany(BATCH_SIZE)
+        if not rows:
+            break
+
+        i = 0
+        for row in rows:
+            # Créer un objet Node avec comme label Artist et les propriétés adéquates
+            n=Node("Artist",idArtist=row[0],primaryName=row[1],birthYear=row[2])
+            importData.append(n)
+            i += 1
+
+        try:
+            create_nodes(graph.auto(), importData, labels={"Artist"})
+            exportedCount += len(rows)
+            print(f"{exportedCount}/{totalCount} names records exported to Neo4j")
+        except Exception as error:
+            print(error)
 
     try:
         print("Indexing Film nodes...")
-        graph.run("CREATE INDEX ON :Film(idFilm)")
+        graph.run("CREATE INDEX IF NOT EXISTS FOR (f:Film) ON (f.idFilm)")
         print("Indexing Name (Artist) nodes...")
-        graph.run("CREATE INDEX ON :Artist(idArtist)")
+        graph.run("CREATE INDEX IF NOT EXISTS FOR (a:Artist) ON (a.idArtist)")
     except Exception as error:
         print(error)
 
@@ -72,7 +97,7 @@ with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
     exportedCount = 0
     cursor.execute("SELECT COUNT(1) FROM tJob")
     totalCount = cursor.fetchval()
-    cursor.execute(f"SELECT idArtist, category, idFilm FROM tJob")
+    cursor.execute("SELECT idArtist, category, idFilm FROM tJob")
     while True:
         importData = { "acted in": [], "directed": [], "produced": [], "composed": [] }
         rows = cursor.fetchmany(BATCH_SIZE)
@@ -84,13 +109,17 @@ with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
             importData[row[1]].append(relTuple)
 
         try:
+            i=0
             for cat in importData:
                 # Utilisez la fonction create_relationships de py2neo pour créer les relations entre les noeuds Film et Name
                 # (les tuples nécessaires ont déjà été créés ci-dessus dans la boucle for précédente)
                 # https://py2neo.org/2021.1/bulk/index.html
                 # ATTENTION: remplacez les espaces par des _ pour nommer les types de relation
                 # A COMPLETER
-                None # Remplacez None par votre code
+                for elem in importData[cat]:
+                    print(elem)
+                    create_relationships(graph.auto(),elem,cat.replace(" ","_"))
+                i+=1
             exportedCount += len(rows)
             print(f"{exportedCount}/{totalCount} relationships exported to Neo4j")
         except Exception as error:
